1d992ac5296649f7f881a97490a9a042c0d66ee9
DianaSemenova <menestrelia199702@gmail.com>
2023-05-22 00:38:28 +0400

    added tasks 5 and 2 js

--- script_HWn/hw8.js
+++ script_HWn/hw8.js
@@ -47,11 +47,71 @@ getCurrentDate(3000, 30000);
 //  так, чтобы приветствие выводилось в консоль не сразу, а спустя 1 секунду. Используйте setTimeout.
 
 function delayForSecond(callback) {
-	setTimeout(() => {
-        console.log (callback());
+    setTimeout(() => {
+        console.log(callback());
     }, 1000);
 }
 
 delayForSecond(function () {
-   console.log('Привет, Глеб!');
+    console.log('Привет, Глеб!');
 })
+
+
+// 5-Посмотрите код. В нём допущена ошибка, и он выводит сообщения не в том порядке:
+// Правильный порядок:
+// Прошла одна секунда
+// Привет, Глеб!
+
+// Функция delayForSecond через 1 секунду пишет в консоль «Прошла одна секунда», 
+// а затем вызывает переданный колбэк
+function delayForSecond(cb) {
+    setTimeout(() => {
+        console.log('Прошла одна секунда');
+        if (cb) { cb(); }
+
+    }, 1000)
+}
+
+// Функция sayHi выводит в консоль приветствие для указанного имени
+function sayHi(name) {
+    console.log(`Привет, ${name}!`);
+}
+
+// Код выше менять нельзя
+
+// Нужно изменить код ниже:
+//delayForSecond(sayHi('Глеб'))
+
+delayForSecond(function () {
+    sayHi('Глеб')
+});
+
+
+// 2 -Реализуйте функцию filter, которая должна работать аналогично методу массива `filter. 
+// За основу возьмите функцию map, которую мы реализовывали на уроке.
+// Чтобы из функции map сделать filter, нужно, в зависимости от результата вызова ruleFunction, 
+// принимать решение о том, добавлять в результирующий массив очередной элемент или нет.
+// Возьмите за основу код ниже. Задание считается выполненным, если два console.log в коде выводят правильное значение:
+
+
+function isPositive(number) {
+    return number > 0;
+}
+function isMale(word) {
+    return word.gender === 'male';
+}
+function filter(arr, ruleFunction) {
+    let arrayNew = arr.filter(el => ruleFunction(el))
+    return arrayNew;
+
+}
+console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+
+const people = [
+    { name: 'Глеб', gender: 'male' },
+    { name: 'Анна', gender: 'female' },
+    { name: 'Олег', gender: 'male' },
+    { name: 'Оксана', gender: 'female' }
+];
+
+console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
