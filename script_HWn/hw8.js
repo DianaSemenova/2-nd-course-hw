e180b0fb95c33d193f1f7b4e80a4c569310b7566
DianaSemenova <menestrelia199702@gmail.com>
2023-05-21 22:59:07 +0400

    added task 3 js

--- script_HWn/hw8.js
+++ script_HWn/hw8.js
@@ -1,12 +1,28 @@
 //1-С помощью метода массива sort отсортируйте массив people по возрастанию возраста и выведите их в консоль.
 const people = [
-   { name: 'Глеб', age: 29 },
-   { name: 'Анна', age: 17 },
-   { name: 'Олег', age: 7 },
-   { name: 'Оксана', age: 47 }
+    { name: 'Глеб', age: 29 },
+    { name: 'Анна', age: 17 },
+    { name: 'Олег', age: 7 },
+    { name: 'Оксана', age: 47 }
 ];
 
 function sortAscending(a, b) {
     return a.age - b.age;
 };
 console.log(people.sort(sortAscending));
+
+
+//3-Напишите программу, которая на протяжении 30 секунд каждые 3 секунды 
+// будет выводить в консоль текущую дату. Последней строкой должно выводиться сообщение «30 секунд прошло».
+
+
+let stopInterval = setInterval(() => {
+    let currentDate = new Date();
+    console.log(currentDate);
+}, 1000 * 3);
+
+setTimeout(() => {
+    clearInterval(stopInterval);
+    console.log('30 секунд прошло');
+}, 1000 * 30);
+
